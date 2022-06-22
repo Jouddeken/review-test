078729cd26dff1fcd516c7d538fb84174cd477f9
Job Ouddeken <job.ouddeken@sentinels.ai>
2022-06-22 13:47:19 +0200

    Update

--- src/dataNormalizer.js
+++ src/dataNormalizer.js
@@ -1,27 +1,3 @@
-const rating = (number) => {
-  if (number < 1) {
-    return '☆☆☆☆☆';
-  }
-  if (number > 1 && number < 2) {
-    return '★☆☆☆☆';
-  }
-  if (number > 2 && number < 3) {
-    return '★★☆☆☆';
-  }
-  if (number > 3 && number < 4) {
-    return '★★★☆☆';
-  }
-  if (number > 4 && number < 5) {
-    return '★★★★☆';
-  }
-  if (number > 5 && number < 6) {
-    return '★★★★★';
-  }
-  if (!number) {
-    return 'No rating';
-  }
-};
-
 export const dataNormalizer = (data) => {
   let x = [];
   for (let i = 0; i < data.length; i++) {
@@ -30,8 +6,6 @@ export const dataNormalizer = (data) => {
       full_name: data[i].first_name.charAt(0).toUpperCase() + data[i].first_name.slice(1) + ' ' + data[i].last_name.charAt(0).toUpperCase() + data[i].last_name.slice(1),
       // Create date of birth
       date_of_birth: Intl.DateTimeFormat('en-GB').format(new Date(data[i].age)),
-      // Get the rating
-      rating: rating(data[i].rating)
     };
     delete newData.age;
     delete newData.name;
--- src/dataNormalizer.test.js
+++ src/dataNormalizer.test.js
@@ -5,13 +5,11 @@ const mockData = [{
   first_name: 'james',
   last_name: 'damon oliver',
   age: '04-08-1985',
-  rating: 3.728823,
 }];
 
 test('should work', () => {
   const result = dataNormalizer(mockData);
   expect(result[0].id).toBe('62b7f6e3');
   expect(result[0].date_of_birth).toBe('08/04/1985');
-  expect(result[0].rating).toBe('★★★☆☆');
   expect(result[0].full_name).toBe('James Damon Oliver');
 });
